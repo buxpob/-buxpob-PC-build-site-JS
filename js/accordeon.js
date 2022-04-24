a4beaa46b440bef4dec675e5c5a9127bd736aee4
buxpob <buxpob.xax@gmail.com>
2022-04-24 13:14:51 +0300

    Добавил показ-скрытие модальный окон и плавный скролл

---
+++ js/accordeon.js
@@ -0,0 +1,12 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+contents.forEach((elem, index) => {
+  const title = elem.querySelector('.program-line__title');
+  const description = elem.querySelector('.program-line__descr');
+
+  title.addEventListener('click', () => {
+    const currentIndex = index;
+    contents.forEach((elem, index) => index !== currentIndex && elem.querySelector('.program-line__descr').classList.remove('active'));       
+    description.classList.toggle('active');    
+  });
+});
---
+++ js/modal.js
@@ -0,0 +1,70 @@
+const modalButtonApplication = document.querySelector('.modal__button');
+const modalButtonCourse = document.querySelector('.course__button');
+const modal = document.querySelector('.modal');
+const modalInner = document.querySelector('.modal__inner');
+
+const createButtonModalClose = () => {
+  const buttonClose = document.createElement('div');
+  buttonClose.classList.add('button__close');
+  buttonClose.innerHTML = 'X';
+  modalInner.prepend(buttonClose);
+  modalInner.style.position = 'relative';
+  buttonClose.style.cssText = `
+    font-size:20px; 
+    position:absolute; 
+    display:inline; 
+    padding:10px; 
+    top:10px; 
+    right:10px; 
+    cursor:pointer;
+`;
+}
+
+createButtonModalClose();
+
+const escKeydown = (evt, el) => {
+  evt.preventDefault(); 
+  if (evt.key  === 'Esc' || evt.key  === 'Escape') {
+    el.style.display = '';
+  }
+  document.removeEventListener('keydown', (evt) => {
+  escKeydown(evt, el);
+  }); 
+}
+
+const clickButtonCloseModal = (el) => {
+  const buttonClose = el.querySelector('.button__close');
+  buttonClose.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    el.style.display = '';
+    document.removeEventListener('keydown', (evt) => {
+    escKeydown(evt, el);
+  }); 
+  }); 
+}
+
+const escKeydownCloseModal = () => {
+  document.addEventListener('keydown', (evt) => escKeydown(evt, modal)); 
+}
+
+modalButtonApplication.addEventListener('click', () => {
+  modal.style.display = 'flex';
+  clickButtonCloseModal(modal);
+  escKeydownCloseModal();
+});   
+
+modalButtonCourse.addEventListener('click', () => {
+  modal.style.display = 'flex';
+  modal.querySelector('.modal__header').innerHTML = 'Заявка на заказ курса';
+  clickButtonCloseModal(modal);
+  escKeydownCloseModal();
+});
+
+modal.addEventListener('click', (evt) => {
+  const modelContent = evt.target.closest('.modal__inner');
+  if (!modelContent) {
+    modal.style.display = '';
+  }
+})
+
+
